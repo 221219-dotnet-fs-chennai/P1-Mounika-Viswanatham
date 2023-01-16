56b810705ac3c83af0dd3f299f95ac5f1538d78d
ViswanathamMounika <mounikav.199@gmail.com>
2023-01-16 09:59:30 +0530

    code for Project1

---
+++ TrainerProject/Trainer/GetTrainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainer
+{
+    internal class GetTrainer : IMenu
+    {
+        public void Display()
+        {
+            Console.WriteLine("[0] Login");
+            Console.WriteLine("[1] Signup");
+            Console.WriteLine("[2] Menu");
+
+        }
+        public string UserChoice()
+        {
+            Console.WriteLine("Enter Your Option");
+            string? userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "0":
+                    return "Login";
+                case "1":
+                    return "Signup";
+                case "2":
+                    return "Menu";
+                default:
+                    Console.WriteLine("OOPS Please enter a valid input");
+                    Console.WriteLine("Please press enter to continue");
+                    Console.ReadLine();
+                    return "Login";
+            }  }
+    }
+}
---
+++ TrainerProject/Trainer/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainer
+{
+    internal class Menu : IMenu
+    {
+        public void Display()
+        {
+            Console.WriteLine("Welcome Trainer  ");
+            Console.WriteLine("[0] Exit");
+            Console.WriteLine("[1] Signup");
+            Console.WriteLine("[2] Login");
+        }
+
+        public string UserChoice()
+        {
+            string? userInput = Console.ReadLine();
+            switch(userInput)
+            {
+                case "2":
+                    return "Login";
+                case "1":
+                    return "Signup";
+                case "0":
+                    return "Exit";
+                default:
+                    Console.WriteLine("OOPS Please enter a valid input");
+                    Console.WriteLine("Please press enter to continue");
+                    Console.ReadLine();
+                    return "Menu";
+
+            }
+        }
+    }
+}
